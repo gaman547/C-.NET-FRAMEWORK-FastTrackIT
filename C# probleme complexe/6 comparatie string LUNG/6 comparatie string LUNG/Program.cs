2335846440968c5f5ec0f641896d8de5c5a1ac36
Gabriel Kopacz <48963226+gaman547@users.noreply.github.com>
2019-05-17 17:37:30 +0300

    Add files via upload

---
+++ C# probleme complexe/6 comparatie string LUNG/6 comparatie string LUNG/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_comparatie_string_LUNG
+{
+    class Program
+    {
+        public static string text1, text2 = string.Empty;
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write(" text 1: ");
+            Console.ResetColor();
+            text1 = Console.ReadLine().TrimStart().TrimEnd();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" text 2: ");
+            Console.ResetColor();
+            text2 = Console.ReadLine();
+            Console.WriteLine();
+
+            Comparatie.VerificareSegment();
+            Console.ReadKey();
+        }
+    }
+}
